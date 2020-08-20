6a238302ac8302564ce5ae6b15f622dffe89a6d1
Taras Dutchak <tarasdutchak90@gmail.com>
2020-08-20 20:19:00 +0300

    film article page - desk and adaptive

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $('.menu__icon .openmenu').click(function(){
+$(document).ready(function () {
+    $('.menu__icon .openmenu').click(function () {
         $(this).hide();
         $('.menu__icon .closemenu').show();
         $('.header__menu ul').addClass('show');
         $('.header__menu > span').addClass('hide');
     });
-    $('.menu__icon .closemenu').click(function(){
+    $('.menu__icon .closemenu').click(function () {
         $(this).hide();
         $('.menu__icon .openmenu').show();
         $('.header__menu ul').removeClass('show');
@@ -13,12 +13,24 @@ $(document).ready(function(){
     });
 
     if ($(window).width() < 768) {
-        $('.menu__icon .openmenu').click(function(){
-          $('.mobwrap-menu').addClass('show');
+        $('.menu__icon .openmenu').click(function () {
+            $('.mobwrap-menu').addClass('show');
         });
-        $('.menu__icon .closemenu').click(function(){
+        $('.menu__icon .closemenu').click(function () {
             $('.mobwrap-menu').removeClass('show');
         });
     }
+
+
+    // player
+    if($('*').is('.js-player')) {
+        const players = Plyr.setup('.js-player', {
+            title: 'Example Title',
+        });
+    }
     
+
+    // const player = new Plyr('#player', {
+    //     title: 'Example Title',
+    //   });
 });
